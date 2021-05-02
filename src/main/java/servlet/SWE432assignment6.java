5bb6c9d5668924d565c185679b2ce88196df04fd
Du Tran <72167289+dtran22@users.noreply.github.com>
2021-05-02 02:31:30 -0400

    Update SWE432assignment6.java

--- src/main/java/servlet/SWE432assignment6.java
+++ src/main/java/servlet/SWE432assignment6.java
@@ -23,7 +23,7 @@ import javax.servlet.annotation.WebServlet;
     )
 
 public class SWE432assignment6 extends HttpServlet {
-  static enum Data {NAMES, RESULT};
+  static enum Data {NAMES, RANDOM, REPLACEMENT, WOREPLACEMENT, SORTED, REVERSED, RESULT};
   static String RESOURCE_FILE = "names.json";
 
   static String Domain = "";
@@ -91,27 +91,37 @@ public class SWE432assignment6 extends HttpServlet {
     }
 
     public String getAllAsHTMLTable(Entries entries) {
-      StringBuilder htmlOut = new StringBuilder("<table>");
-      htmlOut.append("<tr><th>Names</th><th>Result</th></tr>");
+      StringBuilder htmlOut = new StringBuilder("<table style=\"border-collapse: seperate; border-spacing: 0 15px;\">");
+      htmlOut.append("<tr><th style=\"padding-right: 30px\">Names</th><th>Result</th></tr>");
       if(entries == null
           || entries.entries == null || entries.entries.size() == 0){
-        htmlOut.append("<tr><td>No entries yet.</td></tr>");
+        htmlOut.append("<tr><td style=\"padding-right: 50px\">No entries yet.</td></tr>");
       }else{
         for(Entry entry: entries.entries){
            htmlOut.append(
-           "<tr><td>"+entry.names+"</td><td>"+entry.result+"</td></tr>");
+           "<tr><td style=\"padding-right: 50px\">"+entry.names+"</td><td>"+entry.result+"</td></tr>");
         }
       }
       htmlOut.append("</table>");
       return htmlOut.toString();
     }
   }
-
+  ArrayList<String> selected = new ArrayList<String>();
   @Override
   public void doPost (HttpServletRequest request, HttpServletResponse response)
      throws ServletException, IOException
      {
       String names = request.getParameter(Data.NAMES.name());
+      String random = request.getParameter(Data.RANDOM.name());
+      String wReplacement = request.getParameter(Data.REPLACEMENT.name());
+      String woReplacement = request.getParameter(Data.WOREPLACEMENT.name());
+      String sorted = request.getParameter(Data.SORTED.name());
+      String reversed = request.getParameter(Data.REVERSED.name());
+      String randomResult = new String();
+      String woReplacementResult = new String();
+      String wReplacementResult = new String();
+      String sortedResult = new String();
+      String reversedResult = new String();
       String result = new String();
 
       String error = "";
@@ -121,33 +131,59 @@ public class SWE432assignment6 extends HttpServlet {
         result = "";
       }
       else{
-
-      //if (request.getParameter("randomSelect") != null) {
-        String[] namesArr = names.split(" ");
-        Random r = new Random();
-        int i = r.nextInt(namesArr.length);
-        result = namesArr[i];
+        if (random != null) {
+          String[] namesArr = names.split(" ");
+          Random r = new Random();
+          int i = r.nextInt(namesArr.length);
+          randomResult = "Random result: " + namesArr[i];
+          result += randomResult + "<br>";
+        }
+        if(wReplacement != null) {
+          String[] namesArr = names.split(" ");
+          Random r = new Random();
+          int i = r.nextInt(namesArr.length);
+          if(namesArr.length == selected.size()) {
+            wReplacementResult = "Random with replacement result: All names have been selected.";
+          }
+          else if(selected.contains(namesArr[i])) {
+            while(selected.contains(namesArr[i]) == true){
+              i = r.nextInt(namesArr.length);
+            }
+            wReplacementResult = "Random with replacement result: " + namesArr[i];
+            selected.add(namesArr[i]);
+          }
+          else{
+            wReplacementResult = "Random with replacement result: " + namesArr[i];
+            selected.add(namesArr[i]);
+          }
+          result += wReplacementResult + "<br>";
+        }
+        if(woReplacement != null) {
+          String[] namesArr = names.split(" ");
+          Random r = new Random();
+          int i = r.nextInt(namesArr.length);
+          woReplacementResult = "Random without replacement result: " + namesArr[i];
+          result += woReplacementResult + "<br>";
+        }
+        if(sorted != null) {
+          String[] namesArr = names.split(" ");
+          Arrays.sort(namesArr);
+          sortedResult = "Sorted order result: ";
+          for(int i = 0; i < namesArr.length; i++){
+            sortedResult += namesArr[i] + " ";
+          }
+          result += sortedResult + "<br>";
+        }
+        if(reversed != null) {
+          String[] namesArr = names.split(" ");
+          Arrays.sort(namesArr, Collections.reverseOrder());
+          reversedResult = "Reversed sorted order result: ";
+          for(int i = 0; i < namesArr.length; i++){
+            reversedResult += namesArr[i] + " ";
+          }
+          result += reversedResult + "<br>";
+        }
       }
-      //}
-      /**
-      if(request.getParameter("replacementRandom") != null) {
-        String[] namesArr = names.split(" ");
-        Random r = new Random();
-        int i = r.nextInt(namesArr.length);
-      }
-      if(request.getParameter("woReplacementRandom") != null) {
-        String[] namesArr = names.split(" ");
-        Random r = new Random();
-        int i = r.nextInt(namesArr.length);
-      }
-      if(request.getParameter("sorted") != null) {
-        String[] namesArr = names.split(" ");
-        Arrays.sort(namesArr);
-      }
-      if(request.getParameter("reversed") != null) {
-        String[] namesArr = names.split(" ");
-        Arrays.sort(namesArr, Collections.reverseOrder());
-      }*/
       response.setContentType("text/html");
       PrintWriter out = response.getWriter();
       if(error.length() == 0) {
@@ -188,7 +224,7 @@ public class SWE432assignment6 extends HttpServlet {
     out.println("<html>");
     out.println("");
     out.println("<head>");
-    out.println("<title>SWE 432 assignment4</title>");
+    out.println("<title>SWE 432 assignment6</title>");
     out.println("<style>");
     out.println(" body {");
     out.println("   background-color: lightblue;");
@@ -251,21 +287,21 @@ public class SWE432assignment6 extends HttpServlet {
     out.println(" <input type=\"text\" id=\"name\" name=\"" + Data.NAMES.name() + "\" value=\"" + names + "\"><br><br>");
     out.println(" <div class=\"inner\">");
     out.println(" <label for=\"random\">");
-    out.println(" <input type=\"checkbox\" id=\"random\" name=\"randomSelect\" value=\"randomSelect\">");
+    out.println(" <input type=\"checkbox\" id=\"random\" name=\"" + Data.RANDOM.name() + "\" value=\"randomSelect\">");
     out.println(" Random select</label><br>");
     out.println(" <label for=\"wreplacement\">");
-    out.println(" <input type=\"checkbox\" id=\"wreplacement\" name=\"replacementRandom\" value=\"replacementRandom\">");
+    out.println(" <input type=\"checkbox\" id=\"wreplacement\" name=\"" + Data.REPLACEMENT.name() + "\" value=\"replacementRandom\">");
     out.println(" Random select with replacement</label><br>");
     out.println(" <label for=\"woreplacement\">");
-    out.println(" <input type=\"checkbox\" id=\"woreplacement\" name=\"woReplacementRandom\" value=\"woReplacementRandom\">");
+    out.println(" <input type=\"checkbox\" id=\"woreplacement\" name=\"" + Data.WOREPLACEMENT.name() + "\" value=\"woReplacementRandom\">");
     out.println(" Random select without replacement</label><br>");
     out.println(" <label for=\"sorted\">");
-    out.println(" <input type=\"checkbox\" id=\"sorted\" name=\"sorted\" value=\"sorted\">");
+    out.println(" <input type=\"checkbox\" id=\"sorted\" name=\"" + Data.SORTED.name() + "\" value=\"sorted\">");
     out.println(" Sorted order</label><br>");
     out.println(" <label for=\"reversed\">");
-    out.println(" <input type=\"checkbox\" id=\"reversed\" name=\"reversed\" value=\"reversed\">");
+    out.println(" <input type=\"checkbox\" id=\"reversed\" name=\"" + Data.REVERSED.name() + "\" value=\"reversed\">");
     out.println(" Reversed order</label><br><br>");
-    out.println(" <input type=\"text\" id=\"result\" name=\"" + Data.RESULT.name() + "\" value=\"" + result + "\"><br><br>");
+    //out.println(" <input type=\"text\" id=\"result\" name=\"" + Data.RESULT.name() + "\" value=\"" + result + "\"><br><br>");
     out.println(" </div><br><br>");
     out.println(" <input class=\"center\" type=\"button\" onclick=\"reset()\" value=\"Clear\">");
     out.println(" <br><br>");
